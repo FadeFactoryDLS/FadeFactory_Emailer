da4b5a66e1cfd575e4f521106a4787c37d483ba6
Snaflus <casp909i@edu.easj.dk>
2024-05-09 08:57:33 +0200

    Basic auth (RFC 2617) implemented

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from "express";
 import { sendMail } from "./services/mailService";
 import dotenv from "dotenv";
 import Email from "./models/emailModel";
+import { BasicStrategy } from "passport-http";
+import passport from "passport";
 
 dotenv.config();
 
@@ -10,43 +12,66 @@ const port = 8081;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/send-mail/promotion", async (req: Request, res: Response) => {
-  const emailData: Email = req.body;
+const user = {
+  username: process.env.SIMPLE_LOGIN_USERNAME,
+  password: process.env.SIMPLE_LOGIN_PASSWORD,
+};
 
-  if (emailData) {
-    const to: string = emailData.emailAddress;
-    const subject: string = emailData.subject;
-    const mailTemplate: string = "email_promotion";
-    const context: object = {
-      name: emailData.name,
-      subject: emailData.subject,
-      message: emailData.message,
-    };
+passport.use(
+  new BasicStrategy(function (username, password, cb) {
+    if (username === user.username && password === user.password) {
+      return cb(null, user);
+    } else {
+      return cb(null, false);
+    }
+  })
+);
 
-    sendMail(to, subject, mailTemplate, context);
+app.post(
+  "/send-mail/promotion",
+  passport.authenticate("basic", { session: false }),
+  async (req: Request, res: Response) => {
+    const emailData: Email = req.body;
 
-    res.sendStatus(200);
+    if (emailData) {
+      const to: string = emailData.emailAddress;
+      const subject: string = emailData.subject;
+      const mailTemplate: string = "email_promotion";
+      const context: object = {
+        name: emailData.name,
+        subject: emailData.subject,
+        message: emailData.message,
+      };
+
+      sendMail(to, subject, mailTemplate, context);
+
+      res.sendStatus(200);
+    }
   }
-});
+);
 
-app.post("/send-mail/reminder", async (req: Request, res: Response) => {
-  const emailData: Email = req.body;
+app.post(
+  "/send-mail/reminder",
+  passport.authenticate("basic", { session: false }),
+  async (req: Request, res: Response) => {
+    const emailData: Email = req.body;
 
-  if (emailData) {
-    const to: string = emailData.emailAddress;
-    const subject: string = emailData.subject;
-    const mailTemplate: string = "email_reminder";
-    const context: object = {
-      name: emailData.name,
-      subject: emailData.subject,
-      message: emailData.message,
-    };
+    if (emailData) {
+      const to: string = emailData.emailAddress;
+      const subject: string = emailData.subject;
+      const mailTemplate: string = "email_reminder";
+      const context: object = {
+        name: emailData.name,
+        subject: emailData.subject,
+        message: emailData.message,
+      };
 
-    sendMail(to, subject, mailTemplate, context);
+      sendMail(to, subject, mailTemplate, context);
 
-    res.sendStatus(200);
+      res.sendStatus(200);
+    }
   }
-});
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
