94641ed0d51f23b4aaf102e622673016823b55ec
Snaflus <casp909i@edu.easj.dk>
2024-05-09 17:55:10 +0200

    remove unused tests

--- test/app.test.ts
+++ test/app.test.ts
@@ -4,14 +4,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// test("will pass", () => {
-//   console.log("hello world");
-// });
-
-// test("will fail", () => {
-//   throw new Error("fail");
-// });
-
 test("dotenv saved correctly", () => {
   const dotenv_variable = process.env.TESTING_VARIABLE;
   assert.equal(dotenv_variable, "test-passed");
