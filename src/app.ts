ea33aa22bc73693187cbe4f21e34a29ec5a0ff98
Snaflus <casp909i@edu.easj.dk>
2024-05-09 13:33:43 +0200

    default port change

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import passport from "passport";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
